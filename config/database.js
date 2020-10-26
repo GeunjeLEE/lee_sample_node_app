ab45000d4025e6fa9118440d37b35186ec8edfd7
LeeKeunJe <nigasa12@gmail.com>
2020-10-26 22:37:58 +0900

    add test app

---
+++ config/database.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var db_info = {
+    host: '192.168.56.16',
+    port: '3306',
+    user: 'examuser',
+    password: 'pwd123pwd',
+    database: 'db_test'
+}
+
+module.exports = {
+    init: function () {
+        return mysql.createConnection(db_info);
+    },
+    connect: function(conn) {
+        conn.connect(function(err) {
+            if(err) console.error('mysql connection error : ' + err);
+            else console.log('mysql is connected successfully!');
+        });
+    }
+}
